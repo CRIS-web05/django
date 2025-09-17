869e77e237a8c5e3c40019228f77a143673e2801
CRIS-web05 <andresawchicaiza@gmail.com>
2025-09-16 19:01:37 -0500

    version2
    despliegue

---
+++ djnango_crud/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from tasks import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("signup/", views.signup, name="signup"),
+    path("tasks/", views.tasks, name="tasks"),
+    path("signin/", views.signin, name="signin"),
+    path("signout/", views.signout, name="signout"),
+    path("tasks/create/", views.create_task, name="create_task"),
+    path("tasks/edit/<int:task_id>/", views.editar_task, name="editar_task"),
+    path("tasks/delete/<int:task_id>/", views.eliminar_task, name="eliminar_task"),
+    path("tasks/<int:task_id>/", views.detail_task, name="detail_task"),
+    path("tasks/select/<int:task_id>/", views.select, name="select"),
+
+]
---
+++ tasks/views.py
@@ -0,0 +1,152 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from .forms import TaskForm
+from .models import Task
+
+
+# Create your views here.
+def home(request):
+    return render(request, 
+                  "home.html",
+                  {"form":TaskForm})
+
+def tasks(request):
+
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, "tasks.html", {"tasks": tasks})
+
+    tasks = Task.objects.filter(user=request.user,
+                                 datecompleted__isnull=True)
+    return render(request, "tasks.html",
+                  {"tasks":tasks})
+
+def select(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == "POST":
+        return redirect('tasks')
+    return render(request, 'select.html', {'task': task})
+
+def detail_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    return render(request, "detail_task.html", {"task": task})
+
+def create_task(request):
+    if request.method == "GET":
+        return render(request, 
+                "create_task.html",
+                   {"form": TaskForm})
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect("tasks")
+        except ValueError:
+            return render(request, 
+                    "create_task.html",
+                   {"form": TaskForm(),
+                    "error":"Por favor ingrese datos validos"})
+def editar_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+
+    if request.method == "GET":
+        form = TaskForm(instance=task)
+        return render(request, "editar_task.html", {"form": form})
+    else:
+        try:
+            form = TaskForm(request.POST, instance=task)
+            if form.is_valid():
+                form.save()
+                return redirect("tasks")
+            else:
+                return render(request, "editar_task.html", {"form": form, "error": "Datos inválidos"})
+        except ValueError:
+            return render(request, "editar_task.html", {"form": form, "error": "Error al editar la tarea"})
+
+def eliminar_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+
+    if request.method == "POST":
+        task.delete()
+        return redirect("tasks")
+
+    return render(request, "eliminar_confirmar.html", {"task": task})
+
+
+def create_task(request):  
+   if request.method == "GET":
+       return render(request, 
+                     "create_task.html",
+                     {"form":TaskForm})
+   else:
+       try:
+           form = TaskForm(request.POST)
+           new_task = form.save(commit=False)
+           new_task.user = request.user
+           new_task.save() 
+           return redirect("tasks")
+       except ValueError:
+           return render(request, 
+                         "create_task.html",
+                         {"form":TaskForm(),
+                          "error":"Por favor proporciona datos validos"})
+    
+def signout(request):
+    logout(request)
+    return redirect("home")
+ 
+     
+       
+def signout(request):
+    logout(request)
+    return redirect("home")
+
+def signin(request):
+    if request.method == "GET":
+        return render(request, 
+                  "signin.html",
+                  {"form":AuthenticationForm})
+    else:
+        user = authenticate(request,
+                            username=request.POST['username'],
+                            password=request.POST['password'])
+        if user is None:
+            return render(request,
+                          'signin.html',
+                          {"form": AuthenticationForm(),
+                           "error":"Usuario o contraseña incorrecta"})
+        else:
+            login(request, user)
+            return redirect("tasks")
+
+def signup(request):
+    if request.method == "GET":
+        return render(request, 
+                  "signup.html",
+                  {"form":UserCreationForm})
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect("tasks")
+
+            except IntegrityError:
+                return render(request,
+                              'signup.html',
+                              {"form": UserCreationForm(),
+                               "error":"Error al crear el usuario"})
+                
+        else:
+                 return render(request,
+                              'signup.html',
+                              {"form": UserCreationForm(),
+                               "error":"Error, Las contraseñas no coinciden"})
